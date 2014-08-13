8fcf78afaa39e8ffff4dddf27e4ddcfe394dddad
kiu <kiu@gmx.net>
2014-08-13 20:50:50 +0200

    Added some mobile headers

--- webroot/tpl/tpl_header.php
+++ webroot/tpl/tpl_header.php
@@ -5,9 +5,11 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <meta name="description" content="BRAVE Intel Map">
     <meta name="author" content="kiu Nakamura">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
     <link rel="shortcut icon" href="favicon.png">
     <title>BRAVE Intel Map</title>
     <link href="css/bootstrap-cyborg.min.css" rel="stylesheet">
